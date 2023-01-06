11ca2ea35813047ee878bc1e9816259f3a24a82d
Vikram Gupta <basecs101@gmail.com>
2023-01-06 08:49:54 +0530

    Adding AnonymousClassExample for showcasing the anonymous class creation and method calling.

---
+++ inner-and-anonymous-classes-examples/src/AnonymousClassExample.java
@@ -0,0 +1,37 @@
+/**
+ * Anonymous inner class example.
+ */
+interface ExampleInterface {
+        void display();
+}
+
+//class TestImp implements Test{
+//    @Override
+//    public void display() {
+//        System.out.println("display");
+//    }
+//}
+public class AnonymousClassExample {
+
+    public static void main(String[] args) {
+
+        /*
+            1. The left side type should be an Interface e.g. Test exampleInterface
+            2. The right side must be an anonymous class body eg. new Test() { ... };
+            {...} this is anonymous class that implements ExampleInterface interface
+            and also creates its object and finally assigns the address to exampleInterface
+            variable.
+         */
+        ExampleInterface exampleInterface = new ExampleInterface() {
+            @Override
+            public void display() {
+                System.out.println("Hello from anonymous class method");
+            }
+        };
+//      Test exampleInterface = () -> System.out.println("Hello from anonymous class display method");
+        exampleInterface.display();
+    }
+}
+
+
+
