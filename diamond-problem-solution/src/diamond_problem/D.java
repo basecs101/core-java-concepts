d72176a731bebf932922a2d4a1aefc20200a5bad
Vikram Gupta <vikramguptavit@gmail.com>
2023-01-23 08:13:50 +0530

    update diamond problem

--- diamond-problem-solution/src/diamond_problem/D.java
+++ diamond-problem-solution/src/diamond_problem/D.java
@@ -1,14 +1,5 @@
 package diamond_problem;
-//single line comment
 
-/*
- multiline
- comment
- hello
-    from
-         multi line
-                comment
- */
 //public class D extends C,B{
 //    @Override
 //    public void displayName() {
--- pass-by-value/src/Main.java
+++ pass-by-value/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
 
-    static void swap(String a, String b){
+    static void swap(String a, String b) {
         String temp = a;
         a=b;
         b=temp;
 
-        System.out.println("a = "+a +" b = "+b);
+        System.out.println("a = "+a.hashCode() +" b = "+b.hashCode());
 
     }
     public static void main(String[] args) {
-        String x="X"; //x(reference or pointer) holds address of "X" object
-        String y="Y"; //y(reference or pointer) also holds address of "Y" object
-        System.out.println("Before swap x = "+x +" y = "+y);
+        String x="P"; //x(reference or pointer) holds address of "X" object
+        String y="Q"; //y(reference or pointer) also holds address of "Y" object
+        System.out.println("Before swap x = "+x.hashCode() +" y = "+y.hashCode());
         swap(x,y);//values of x and y pass to swap method
-        System.out.println("After swap x = "+x +" y = "+y);
+        System.out.println("After swap x = "+x.hashCode() +" y = "+y.hashCode());
     }
 }
